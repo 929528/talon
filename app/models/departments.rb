3ebe310849e6497c6311dec50b9a94273a12980e
oleg <929528@gmail.com>
2013-08-12 18:29:37 +0300

    Remove trash

--- app/models/departments.rb
+++
@@ -1,6 +0,0 @@
-class Departments < ActiveRecord::Base
-  attr_accessible :name
-  belongs_to :organization
-
-  validates :name , presence: true, length: {maximum: 50, minimum: 4}
-end
--- app/models/organizations.rb
+++
@@ -1,7 +0,0 @@
-class Organizations < ActiveRecord::Base
-  attr_accessible :name
-  belongs_to :user
-  has_many :department
-
-  validates :name , presence: true, length: {maximum: 50, minimum: 4}, uniqueness: true
-end
