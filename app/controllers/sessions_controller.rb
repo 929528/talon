67997fd85d9af470552d2ca5a30651481cf84bd1
oleg <929528@gmail.com>
2013-08-13 18:30:51 +0300

    BaseSessionFinish

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by_name(params[:session][:name])
+		if user && user.authenticate(params[:session][:password])
+			flash.now[:notice] = "Добро пожаловать."
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = 'Неверное имя пользователя или пароль.' 
+			render 'new'
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Talon::Application.routes.draw do
-  get "users/new"
+  resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +55,7 @@ Talon::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
